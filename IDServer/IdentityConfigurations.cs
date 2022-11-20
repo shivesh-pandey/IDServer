f0a1549213e8c341acae67571aa9d8e21d154aba
spandey <spandey@SURESH-W10>
2022-11-19 23:29:34 -0500

    Add project files.

---
+++ IDServer/IdentityConfigurations.cs
@@ -0,0 +1,64 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Security.Claims;
+
+namespace IDServer
+{
+    public class IdentityConfiguration
+    {
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "101",
+                    Username = "shivesh",
+                    Password = "pandey",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Shivesh Pandey"),
+                        new Claim(JwtClaimTypes.GivenName, "Shivesh"),
+                        new Claim(JwtClaimTypes.FamilyName, "Pandey"),
+                        new Claim(JwtClaimTypes.WebSite, "http://codewithshivesh.com"),
+                    }
+                }
+        };
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+        public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+        new ApiScope("myApi.read"),
+        new ApiScope("myApi.write"),
+        };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+            new ApiResource("myApi")
+            {
+                Scopes = new List<string>{ "myApi.read","myApi.write" },
+                ApiSecrets = new List<Secret>{ new Secret("supersecret".Sha256()) }
+            }
+        };
+
+        public static IEnumerable<Client> Clients =>
+    new Client[]
+    {
+        new Client
+        {
+            ClientId = "cwm.client",
+            ClientName = "Client Credentials Client",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedScopes = { "myApi.read" }
+        },
+    };
+    }
+}
---
+++ IDServer/Program.cs
@@ -0,0 +1,41 @@
+using IDServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiResources(IdentityConfiguration.ApiResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddTestUsers(IdentityConfiguration.TestUsers)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Hello World!");
+    });
+});
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
